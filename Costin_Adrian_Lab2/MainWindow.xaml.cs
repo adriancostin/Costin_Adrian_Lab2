b6d8b35fb0296adf50221be4672e1d5b5ff909d3
Adrian Costin <adrian.costin@bitnet.info>
2021-10-17 13:37:32 +0300

    Lab3

--- Costin_Adrian_Lab2/MainWindow.xaml.cs
+++ Costin_Adrian_Lab2/MainWindow.xaml.cs
@@ -20,6 +20,15 @@ namespace Costin_Adrian_Lab2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DoughnutMachine myDoughnutMachine;
+
+        private int mRaisedGlezed;
+        private int mRaisedSugar;
+        private int mFilledLemon;
+        private int mFilledChocolate;
+        private int mFilledVanilla;
+
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -30,9 +39,63 @@ namespace Costin_Adrian_Lab2
 
         }
 
+        private void frmMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            myDoughnutMachine = new DoughnutMachine();
+            myDoughnutMachine.DoughnutComplete += new DoughnutMachine.DoughnutCompleteDelagate(DoughnutCompleteHandler);
+        }
+
+        private void glazedMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            glazedMenuItem.IsChecked = true;
+            sugarMenuItem.IsChecked = false;
+
+            myDoughnutMachine.MakeDoughnuts(DoughnutType.Glazed);
+        }
+
+        private void sugarMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            glazedMenuItem.IsChecked = false;
+            sugarMenuItem.IsChecked = true;
+
+            myDoughnutMachine.MakeDoughnuts(DoughnutType.Sugar);
+        }
+
+        private void DoughnutCompleteHandler()
+        {
+            switch (myDoughnutMachine.Flavor)
+            {
+                case DoughnutType.Glazed:
+                    mRaisedGlezed++;
+                    txtGlazedRaised.Text = mRaisedGlezed.ToString();
+                    break;
+
+                case DoughnutType.Sugar:
+                    mRaisedSugar++;
+                    txtSugarRaised.Text = mRaisedSugar.ToString();
+                    break;
+
+            }
+
+            
+        }
+
+        private void mnuStop_Click(object sender, RoutedEventArgs e)
+        {
+            myDoughnutMachine.Enabled = false;
+        }
+
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
+            this.Close();
+        }
 
+        private void txtQuantity_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (!(e.Key >= Key.D0 && e.Key <= Key.D9))
+            {
+                MessageBox.Show("Puteti introduce doar cifre!", "Input Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
